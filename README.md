a2e07ca8d74ce24886e03dd9beecd6e20438223e
Winston Hartnett <winstonhartnett@gmail.com>
2020-02-15 17:24:36 -0600

    Base structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2020-Malevolent
-2020 code, written in Kotlin, for FRC Infinite Recharge
---
+++ src/main/kotlin/frc/robot/commands/ExampleCommand.kt
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands
+
+import frc.robot.subsystems.ExampleSubsystem
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class ExampleCommand(val m_subsystem: ExampleSubsystem) : CommandBase() {
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param m_subsystem The subsystem used by this command.
+   */
+  init {
+    addRequirements(m_subsystem)
+  }
+
+  // Called when the command is initially scheduled.
+  override fun initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  override fun execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  override fun end(interrupted: Boolean) {
+  }
+
+  // Returns true when the command should end.
+  override fun isFinished(): Boolean {
+    return false
+  }
+}
