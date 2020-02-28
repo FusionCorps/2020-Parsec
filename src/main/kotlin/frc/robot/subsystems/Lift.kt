d48d7891360c1758edede9d5e4f0ae2133156dd3
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-02-27 18:17:23 -0600

    calculatord aksdlnbf

--- src/main/kotlin/frc/robot/subsystems/Lift.kt
+++ src/main/kotlin/frc/robot/subsystems/Lift.kt
@@ -9,6 +9,8 @@ import com.revrobotics.CANSparkMaxLowLevel
 import com.revrobotics.ControlType
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.Constants
+import java.lang.Math.atan
+import java.lang.Math.sin
 
 object Lift : SubsystemBase() { // Important note: Spark Max Encoders count 4096 ticks per rev by default
     private val talonSRXExtend = WPI_TalonSRX(Constants.Lift.ID_TALONSRX_EXTEND).apply { // Higher speed motor
@@ -66,5 +68,14 @@ object Lift : SubsystemBase() { // Important note: Spark Max Encoders count 4096
 
     fun setRetractPID(target: Double, control_type: ControlType = ControlType.kVelocity) {
         retractPID.setReference(target, control_type)
+    }
+
+    fun location_calculator(reading1: Double, reading2: Double, m_6672: Int, m_1: Int, m_2: Int, m_bar: Int, delta_sensor: Double): Double {
+        val alpha = atan(0.66/1.41)
+        val theta = atan((reading1-reading2)/delta_sensor)
+
+        val distance = (0.66*(reading1-reading2)*(m_1+m_2+m_bar)/(m_6672*delta_sensor))
+        val height = 2.83 - 1.56*sin(alpha+theta) + (1.41 + distance)* sin(theta)
+        return height
     }
 }
