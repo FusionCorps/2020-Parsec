cf595836628eba95c6eb55bb6b02a05a1411483b
Winston Hartnett <winstonhartnett@gmail.com>
2020-02-29 15:54:42 -0600

    Fix some bugs

--- src/main/kotlin/frc/robot/commands/intake/IntakeRunAt.kt
+++ src/main/kotlin/frc/robot/commands/intake/IntakeRunAt.kt
@@ -2,19 +2,20 @@ package frc.robot.commands.intake
 
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.fusion.motion.ControlMode
+import frc.robot.fusion.motion.MotionConfig
 import frc.robot.subsystems.Intake
 
 class IntakeRunAt(
-    vararg configuration: Any
+    vararg config: MotionConfig
 ) : CommandBase() {
-    val mConfiguration = configuration
+    val mConfig = config
 
     init {
         addRequirements(Intake)
     }
 
     override fun initialize() {
-        Intake.control(*mConfiguration)
+        Intake.control(*mConfig)
     }
 
     override fun end(interrupted: Boolean) {
--- src/main/kotlin/frc/robot/commands/shooter/ShooterRunAt.kt
+++ src/main/kotlin/frc/robot/commands/shooter/ShooterRunAt.kt
@@ -1,18 +1,19 @@
 package frc.robot.commands.shooter
 
 import edu.wpi.first.wpilibj2.command.CommandBase
+import frc.robot.fusion.motion.MotionConfig
 import frc.robot.subsystems.Shooter
 
 class ShooterRunAt(
-    vararg configuration: Any
+    vararg config: MotionConfig
 ) : CommandBase() {
-    val mConfiguration = configuration
+    val mConfig = config
 
     init {
         addRequirements(Shooter)
     }
 
     override fun initialize() {
-        Shooter.control(*mConfiguration)
+        Shooter.control(*mConfig)
     }
 }
