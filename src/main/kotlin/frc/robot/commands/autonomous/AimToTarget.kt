ec5f8eeb0591757ec6ec0b2b9f283651359620d3
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-03-03 16:42:29 -0600

    added vel calc

--- src/main/kotlin/frc/robot/commands/autonomous/AimToTarget.kt
+++ src/main/kotlin/frc/robot/commands/autonomous/AimToTarget.kt
@@ -4,26 +4,15 @@ import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.subsystems.Chassis
 import kotlin.math.abs
-import kotlin.math.absoluteValue
 
 class AimToTarget(chassis: Chassis) : CommandBase() {
     private val mChassis = chassis
 
     private val acceptableError = 0.01
 
-    private val kAim = -0.1
-
-    private val minDistance = 0.05
-
     private val limelightTable = NetworkTableInstance.getDefault().getTable("limelight")
 
     private var tx: Double = 0.0
-
-    private var dReq: Double = 0.0
-
-    private val chassisWidth = 1.0 // meters
-
-    private val wheelDiameter = 0.1 // meters
 
     init {
         addRequirements(mChassis)
@@ -31,8 +20,7 @@ class AimToTarget(chassis: Chassis) : CommandBase() {
 
     override fun initialize() {
         tx = limelightTable.getEntry("tx").getDouble(0.0)
-        if (tx > 0.0) {mChassis.tankDrive(0.5, -0.5)}
-        else {mChassis.tankDrive(-0.5, 0.5)}
+        if (tx > 0.0) { mChassis.tankDrive(0.5, -0.5) } else { mChassis.tankDrive(-0.5, 0.5) }
     }
 
     override fun execute() {
@@ -46,6 +34,6 @@ class AimToTarget(chassis: Chassis) : CommandBase() {
     }
 
     override fun end(interrupted: Boolean) {
-        mChassis.tankDrive(0.0 ,0.0)
+        mChassis.tankDrive(0.0, 0.0)
     }
 }
