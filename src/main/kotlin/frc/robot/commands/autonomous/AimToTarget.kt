5da3ae9c4647d20cd87a7cd5f4f29a7b4e2bce51
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-03-04 18:10:34 -0600

    Fixed aim to target (NEEDS TO BE TUNED)

--- src/main/kotlin/frc/robot/commands/autonomous/AimToTarget.kt
+++ src/main/kotlin/frc/robot/commands/autonomous/AimToTarget.kt
@@ -13,6 +13,16 @@ class AimToTarget(chassis: Chassis) : CommandBase() {
     private val limelightTable = NetworkTableInstance.getDefault().getTable("limelight")
 
     private var tx: Double = 0.0
+    private val aimConstant = -0.01
+
+    private val greenZone = 15.5 // Zone where we can shoot
+
+    private var ty: Double = 0.0 // Assumes centered cross hairs
+    private val distanceConstant = -0.01
+
+    private var steeringAdjust = 0.0
+    private val minimumCommand = 0.03
+    private val kAim = 0.02
 
     init {
         addRequirements(mChassis)
@@ -20,17 +30,24 @@ class AimToTarget(chassis: Chassis) : CommandBase() {
 
     override fun initialize() {
         tx = limelightTable.getEntry("tx").getDouble(0.0)
-        if (tx > 0.0) { mChassis.tankDrive(0.5, -0.5) } else { mChassis.tankDrive(-0.5, 0.5) }
+        ty = limelightTable.getEntry("ty").getDouble(0.0)
     }
 
     override fun execute() {
         tx = limelightTable.getEntry("tx").getDouble(0.0)
+        ty = limelightTable.getEntry("ty").getDouble(0.0)
+        if (abs(tx) > 1.0) {
+            steeringAdjust = kAim * tx
+        }
+        else {
+            steeringAdjust = kAim * tx  + minimumCommand
+        }
+
+        mChassis.tankDrive(-steeringAdjust, steeringAdjust)
+
     }
     override fun isFinished(): Boolean {
-        if (abs(tx) < acceptableError) {
-            return true
-        }
-        return false
+        return (abs(tx) < acceptableError)
     }
 
     override fun end(interrupted: Boolean) {
