00bd027b87afb532fd3cae67b4631e655706bccb
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-12-13 10:40:14 -0600

    Update Chassis.kt

--- src/main/kotlin/frc/robot/subsystems/Chassis.kt
+++ src/main/kotlin/frc/robot/subsystems/Chassis.kt
@@ -156,14 +156,14 @@ object Chassis : SubsystemBase() { // Start by defining motors
      *  See https://docs.google.com/document/d/1iRMtpHU5ED9dGsRdzCR48bUF6w-XXB84SJX8Oc-fEmo/edit?usp=sharing
      *  for derivation
      *
-     *  Command: forward, strafe are wheel speeds (percentage for now)
-     *           rotation is clockwise
+     *  Command: forward, strafe are wheel speeds
+     *           rotation is counterclockwise
      *
      *  Axis motors are configured to turn counterclockwise (we install them upside down)
      */
 
     fun swerveDrive(foward: Double, strafe: Double, rotation: Double) {
-        
+
     }
 
 }
