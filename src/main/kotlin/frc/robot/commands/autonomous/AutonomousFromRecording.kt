cf217e8f5ddb9d6eecc777d6e8b3d390f1f73b5f
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2021-03-23 17:53:13 -0500

    tries

--- src/main/kotlin/frc/robot/commands/autonomous/AutonomousFromRecording.kt
+++ src/main/kotlin/frc/robot/commands/autonomous/AutonomousFromRecording.kt
@@ -5,29 +5,21 @@ import frc.robot.commands.chassis.ChassisDriveAmt
 import mu.KotlinLogging
 import java.io.BufferedReader
 import java.io.FileReader
+import java.io.FileWriter
 
 
-
-
-class AutonomousFromRecording {
+class AutonomousFromRecording : SequentialCommandGroup() {
     var csvReader = BufferedReader(FileReader("test.csv"))
     var dataArray = mutableListOf(listOf("0.0", "0.0"))
     var lineCurrent = listOf("0.0, 0.0")
+    val csvWriter = FileWriter("test.csv")
+    
 
 
 
     init {
-//        TODO: Get this working
-//        while (csvReader.readLine() != null) {
-//            dataArray.add(csvReader.readLine().split(","))
-//        }
-//
-//
-//        for (row in dataArray) {
-//            addCommands(ChassisDriveAmt(row[0].toDouble(), row[1].toDouble(), 0.02))
-//        }
-//
-//        addCommands(AutonomousPrintTest(dataArray[0][0]))
-//
+
+
+
     }
 }
